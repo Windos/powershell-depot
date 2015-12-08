fc543b35d5181083dd2981d61f89092c15cdcadc
Joshua King <windosbk+github@gmail.com>
2015-12-08 14:49:42 +1300

    Scripts used to generate my LCTV splash image

---
+++ livecoding.tv/Watch-Stream.ps1
@@ -0,0 +1,22 @@
+﻿function Validate-Streamer ($streamer)
+{
+    if ($streamer -eq $false)
+    {
+        $true
+    }
+    else
+    {
+        throw "$streamer is not currently streaming. Please press follow to be notified when they are next live, or check the Livecoding.tv streaming schedule."
+    }
+}
+
+function Watch-Stream
+{
+    Param
+    (
+        [ValidateScript({Validate-Streamer $_})]
+        [string] $Streamer,
+
+        [string] $Service
+    )
+}
