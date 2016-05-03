246c71dd3cd516f4aa8b69c2b22a593acc8c7339
Joshua King <windosbk+github@gmail.com>
2016-05-03 13:13:41 +1200

    moving my snippets file into repo

---
+++ General/snippets.ps1
@@ -0,0 +1,10 @@
+﻿# List of small snippets that I go back to from time to time
+# Too small for their own script file each
+
+break
+
+# Quickly enable all disabled 'training' generic accounts
+Get-ADUser -Filter {Enabled -eq $false -and Name -like 'Training*'} | Enable-ADAccount
+
+# And disable them again... except for that one that someone insists on leaving for testing
+Get-ADuser -Filter {Enabled -eq $true -and Name -like 'Training*' -and Name -notlike '*8'} | Disable-ADAccount
