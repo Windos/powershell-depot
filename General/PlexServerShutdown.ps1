15c3b2866f6103b97d4338928bacd12237ac19a0
Joshua King <windosbk+github@gmail.com>
2016-06-17 20:45:47 +1200

    PlexServer shutdown script (wip)

---
+++ General/PlexServerShutdown.ps1
@@ -0,0 +1,19 @@
+﻿$ScriptBlock = {
+    $URL = 'http://localhost:32400/status/sessions'
+    
+    while ($true)
+    {
+        $PlexStatus = Invoke-RestMethod -Uri $URL
+    
+        if ($PlexStatus.MediaContainer.Size -eq 0)
+        {
+            Stop-Computer
+        }
+    
+        Start-Sleep -Seconds 900
+    }
+}
+
+$JobTrigger = New-JobTrigger -Daily -At '8:30 PM'
+$JobOption = New-ScheduledJobOption -RunElevated
+Register-ScheduledJob -Name 'PlexShutdown' -Trigger $JobTrigger -ScheduledJobOption $JobOption -ScriptBlock $ScriptBlock
