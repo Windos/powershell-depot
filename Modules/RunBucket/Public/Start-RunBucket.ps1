81afb8d5b029cc7c5688d50a60c5ab30c7b312cc
Windos <windosbk+github@gmail.com>
2018-03-23 21:46:42 +1300

    Spoilers! (For #PSHSummit)

--- Modules/RunBucket/Public/Start-RunBucket.ps1
+++ Modules/RunBucket/Public/Start-RunBucket.ps1
@@ -15,10 +15,12 @@ function Start-RunBucket {
         [int] $Iterations = 100
     )
 
-    $ControlResult = Start-TestCaseMeasurement -ScriptBlock $Control -Throttle 25 -Iterations $Iterations
+	$Runtime = [System.Diagnostics.Stopwatch]::StartNew()
+	$ControlResult = Start-TestCaseMeasurement -ScriptBlock $Control -Throttle 25 -Iterations $Iterations
     Start-Sleep -Milliseconds 50
     $VariationResult = Start-TestCaseMeasurement -ScriptBlock $Variation -Throttle 25 -Iterations $Iterations
-
+	$Runtime.Stop()
+	
     $Difference = [PSCustomObject] @{
         Minimum = Measure-RBDifference -Control $ControlResult.Minimum -Variation $VariationResult.Minimum
         Maximum = Measure-RBDifference -Control $ControlResult.Maximum -Variation $VariationResult.Maximum
@@ -31,9 +33,17 @@ function Start-RunBucket {
         Difference = $Difference
     }
 
+	$ToastTitle = 'RunBucket Finished'
+	
     if ($Title) {
         $Params.Add('Title', $Title)
+		$ToastTitle += ":`n$Title"
     }
 
     Start-RBResultDashboard @Params
+	
+	$AppLogo = Get-ChildItem -Path $PSScriptRoot\..\Media\Stopwatch.png
+	$RuntimeText = 'All tests completed in {0} seconds' -f [Math]::Round($Runtime.Elapsed.TotalSeconds, 2)
+	
+	New-BurntToastNotification -AppLogo $AppLogo -Text $ToastTitle, $RuntimeText -Sound Alarm
 }
