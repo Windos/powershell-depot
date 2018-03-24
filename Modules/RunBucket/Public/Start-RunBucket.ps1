278acaf8dd4b4f7aa0e17a587f40530918dc040e
Windos <windosbk+github@gmail.com>
2018-03-24 14:16:01 +1300

    Swapping the toast, long and silent

--- Modules/RunBucket/Public/Start-RunBucket.ps1
+++ Modules/RunBucket/Public/Start-RunBucket.ps1
@@ -44,6 +44,19 @@ function Start-RunBucket {
 	
 	$AppLogo = Get-ChildItem -Path $PSScriptRoot\..\Media\Stopwatch.png
 	$RuntimeText = 'All tests completed in {0} seconds' -f [Math]::Round($Runtime.Elapsed.TotalSeconds, 2)
-	
-	New-BurntToastNotification -AppLogo $AppLogo -Text $ToastTitle, $RuntimeText -Sound Alarm
+
+    $Text1 = New-BTText -Content $ToastTitle
+    $Text2 = New-BTText -Content $RuntimeText
+    
+    $Image1 = New-BTImage -Source $AppLogo -AppLogoOverride -Crop Circle
+    
+    $Audio1 = New-BTAudio -Silent
+    
+    $Binding1 = New-BTBinding -Children $Text1, $Text2 -AppLogoOverride $Image1
+    
+    $Visual1 = New-BTVisual -BindingGeneric $Binding1
+    
+    $Content1 = New-BTContent -Audio $Audio1 -Visual $Visual1 -ActivationType Protocol -Launch 'http://localhost' -Duration Long
+    
+    Submit-BTNotification -Content $Content1
 }
