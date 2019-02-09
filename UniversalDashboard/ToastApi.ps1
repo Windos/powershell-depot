a6221e515a52b9b5d2a42ee7b30e7e359ae11224
Windos <windosbk+github@gmail.com>
2019-02-09 22:34:46 +1300

    Adjusting Toast

--- UniversalDashboard/ToastApi.ps1
+++ UniversalDashboard/ToastApi.ps1
@@ -1,12 +1,12 @@
 $Method = New-UDAuthenticationMethod
-$Token = Grant-UDJsonWebToken -UserName 'Pipeline'
+# $Token = Grant-UDJsonWebToken -UserName 'Pipeline'
 
 $Endpoint = New-UDEndpoint -Url "/toast" -Method "POST" -Endpoint {
-    param ($Build, $Branch, $Project, $Status, $Commit, $Owner, $Uri)
+    param ($Build, $Branch, $Project, $Status, $Commit, $BuildId)
 
     $ShortCommitSha = $Commit.Substring(0,7)
-    $Button = New-BTButton -Content 'Open' -Arguments $Uri
-    New-BurntToastNotification -Text "Build $Build of $Project $Status", "Source: $ShortCommitSha on branch $Branch", "Triggered by: $Owner"  -AppLogo 'https://avatars2.githubusercontent.com/u/39924718?s=460&v=4' -Button $Button
+    $Button = New-BTButton -Content 'Open' -Arguments "https://dev.azure.com/windosnz/CrashTest/_build/results?buildId=$BuildId"
+    New-BurntToastNotification -Text "$Project build $Build $Status", "Source: $ShortCommitSha", "Branch $Branch"  -AppLogo 'https://avatars2.githubusercontent.com/u/39924718?s=460&v=4' -Button $Button
 }
 $Api = Start-UDRestApi -AuthenticationMethod $Method -Endpoint $Endpoint -Port 8888
 
