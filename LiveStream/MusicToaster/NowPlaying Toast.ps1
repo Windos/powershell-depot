6a90c92de58505deb6cf5c29aaef5fbec8becc89
Josh (Windos) King <windosbk+github@gmail.com>
2020-09-19 07:02:39 +1200

    Update NowPlaying Toast.ps1

--- LiveStream/MusicToaster/NowPlaying Toast.ps1
+++ LiveStream/MusicToaster/NowPlaying Toast.ps1
@@ -10,24 +10,34 @@ Register-ObjectEvent -InputObject $FileSystemWatcher -EventName Changed -Action 
     Start-Sleep 1
 
     $MusicInfo = Import-Csv 'D:\MusicBee\Tags.txt' -Delimiter "`t" -Header 'Artist', 'Album', 'Year', 'Title'
-    $AlbumInfo = '{0} - {1} ({2})' -f $MusicInfo.Artist, $MusicInfo.Album, $MusicInfo.Year
+    $AlbumInfo = '{0} ({1})' -f $MusicInfo.Album, $MusicInfo.Year
 
     if ($MusicInfo.Title -and $MusicInfo.Title -ne $Global:LastTitle) {
         $Global:LastTitle = $MusicInfo.Title
 
         $Header = New-BTText -Text 'Now Playing'
-        $Title = New-BTText -Text $MusicInfo.Title
-        $Album = New-BTText -Text $AlbumInfo
+
 
         $LogoPath = Join-Path -Path (Get-Module BurntToast -ListAvailable)[0].ModuleBase -ChildPath 'Images\BurntToast.png'
         $AppLogo = New-BTImage -Source $LogoPath -AppLogoOverride -Crop Circle
 
         $ImagePath = 'D:\MusicBee\CoverArtwork.jpg'
-        $ToastImage = New-BTImage -Source $ImagePath -RemoveMargin
+        $ToastImage = New-BTImage -Source $ImagePath -RemoveMargin -Align Right
+
+        $TitleLabel = New-BTText -Text 'Title:' -Style Base
+        $AlbumLabel = New-BTText -Text 'Album:' -Style Base
+        $ArtistLabel = New-BTText -Text 'Artist:' -Style Base
+
+        $Title = New-BTText -Text $MusicInfo.Title -Style BaseSubtle
+        $Album = New-BTText -Text $MusicInfo.Artist -Style BaseSubtle
+        $Artist = New-BTText -Text $AlbumInfo -Style BaseSubtle
+
+        $Column1 = New-BtColumn -Children $TitleLabel, $AlbumLabel, $ArtistLabel
+        $Column2 = New-BtColumn -Children $Title, $Album, $Artist
 
         $Audio1 = New-BTAudio -Silent
 
-        $Binding1 = New-BTBinding -Children $Header, $Title, $Album, $ToastImage -AppLogoOverride $AppLogo
+        $Binding1 = New-BTBinding -Children $Header, $ToastImage -Column $Column1, $Column2 -AppLogoOverride $AppLogo
         $Visual1 = New-BTVisual -BindingGeneric $Binding1
         $Content1 = New-BTContent -Visual $Visual1 -Audio $Audio1 -Duration Long
 
